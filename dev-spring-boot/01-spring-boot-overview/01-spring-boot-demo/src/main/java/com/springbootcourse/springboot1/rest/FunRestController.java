bfd66a8e79f3c46f1accc9792c89eaedcd8f31c8
Ahmet Aksünger <116587797+AhmetAksunger@users.noreply.github.com>
2023-04-12 17:43:24 +0300

    Add files via upload

---
+++ dev-spring-boot/01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/springbootcourse/springboot1/rest/FunRestController.java
@@ -0,0 +1,18 @@
+package com.springbootcourse.springboot1.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // expose "/" that returns "Hello World"
+
+    @GetMapping("/")
+    public String sayHello(){
+
+        return "Hello World";
+
+    }
+
+}
