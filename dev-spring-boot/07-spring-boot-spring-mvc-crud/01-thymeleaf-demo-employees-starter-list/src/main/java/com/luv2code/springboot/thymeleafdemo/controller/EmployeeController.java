eeeb92dc79252e52e1f63f994fc7021112b17b41
Ahmet Aksünger <116587797+AhmetAksunger@users.noreply.github.com>
2023-04-17 17:08:10 +0300

    Add Employee

--- dev-spring-boot/07-spring-boot-spring-mvc-crud/01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ dev-spring-boot/07-spring-boot-spring-mvc-crud/01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -30,8 +32,27 @@ public class EmployeeController {
 		// add to the spring model
 		theModel.addAttribute("employees", theEmployees);
 
-		return "list-employees";
+		return "/employees/list-employees";
 	}
+
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel){
+
+		Employee employee = new Employee();
+		theModel.addAttribute("employee",employee);
+
+		return "employees/employees-form";
+	}
+
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee employee){
+
+		employeeService.save(employee);
+
+		return "redirect:/employees/list";
+
+	}
+
 }
 
 
--- dev-spring-boot/07-spring-boot-spring-mvc-crud/01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ dev-spring-boot/07-spring-boot-spring-mvc-crud/01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -4,8 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 	// that's it ... no need to write any code LOL!
+
+    List<Employee> findAllByOrderByFirstNameAsc();
 	
 }
